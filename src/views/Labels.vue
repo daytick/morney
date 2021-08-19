7df2fd771b2036d29d436c5f61e823eb7c416a43
daytick <luyi95@foxmail.com>
2021-08-19 22:33:10 +0800

    创建三个功能页面、创建全局组件<Nav/>、创建404页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>标签
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Labels',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>记账
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Money',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
